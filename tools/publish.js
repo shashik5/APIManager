fa1c9869e1f08d169515653d2ca07e9541e7e069
scorpionsk5 <sashikumar148@gmail.com>
2016-10-03 23:36:03 +0530

    updateed publish.js

--- tools/publish.js
+++ tools/publish.js
@@ -1,8 +1,25 @@
-﻿(function (require) {
+﻿(function (require, process) {
 
     var fs = require('fs'),
-        minify = require('minify');
+        minify = require('minify'),
+        path = require('path'),
+        dir = __dirname.replace(/\\/g, '/'),
+        startBuild = function (basePath) {
+            console.log(basePath);
+        };
 
+    try {
+        var basePath = process.argv[2] ? process.argv[2].replace('"', '') : dir;
 
+        fs.readdir(dir.replace('tools', ''), function (err, items) {
+            items.forEach(function (item) {
+                console.log(item);  // TODO: group by file type and folders and proceed with logic to minify.
+            });
+        });
+        startBuild(basePath);
+    }
+    catch (err) {
+        console.log(err);
+    }
 
-})(require);
+})(require, process);
