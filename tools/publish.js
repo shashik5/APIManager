ee3a9b36471fe456a2a78f22888f150bcdef7583
Shashikumar D <scorpion.sk5@hotmail.com>
2016-10-04 15:31:51 +0530

    added comments to publish.js

--- tools/publish.js
+++ tools/publish.js
@@ -1,79 +1,113 @@
 ﻿(function (require, process) {
 
+    // Load required modules.
     var fs = require('fs'),
         minifyJs = require('minify'),
         pathJs = require('path'),
+
+        // Source directory.
         dir = __dirname.replace(/\\/g, '/'),
         sourcePath = dir.replace('tools', ''),
+
+        // Array of folder names which should not be published.
         foldersToIgnore = ['node_modules', 'build', '.git', 'tools'],
-        filesToIgnore = ['publish.bat', '.gitattributes', '.gitignore'],
+
+        // Array of file names which should not be published.
+        filesToIgnore = ['publish.bat', '.gitattributes', '.gitignore', 'build.bat'],
+
+        // Method to handle log messages.
         logger = function (msg) {
             console.log(msg);
         },
+
+        // Method to check if directory exists alse it creates it.
         createDirectory = function (folderPath) {
             if (!fs.existsSync(folderPath)) {
                 fs.mkdirSync(folderPath);
+                logger(folderPath + ' - directory created because it didn\'t exist.');
             };
         },
+
+        // Method to check if item at end of path is an directory.
         isDirectory = function (path) {
             return fs.statSync(path).isDirectory();
         },
+
+        // Method to check if file is a javascript file.
         isJsFile = function (fileName) {
             return fileName.match(/\.js$/i);
         },
+
+        // Method to loop through folders and publish files.
         processFolder = function (src, dest) {
-            fs.readdir(src, function (err, items) {
-                items.forEach(function (item) {
-                    var newDest = pathJs.join(dest, item),
-                        newSrc = pathJs.join(src, item);
+            fs.readdir(src, function (err, items) { // Read items in source folder.
+                if (err) {
+                    logger(err.message);
+                }
+                else {
+                    items.forEach(function (item) {// Loop through items in source folder.
+                        var newDest = pathJs.join(dest, item),
+                            newSrc = pathJs.join(src, item);
 
-                    if (isDirectory(newSrc)) {
-                        if (foldersToIgnore.indexOf(item) == -1) {
-
-                            createDirectory(newDest);
-                            processFolder(newSrc, newDest);
+                        if (isDirectory(newSrc)) {
+                            // Executes if item is a directory.
+                            if (foldersToIgnore.indexOf(item) == -1) {
+                                // Create folder if doesn't exist in destination folder.
+                                createDirectory(newDest);
+                                // Loop through the current folder.
+                                processFolder(newSrc, newDest);
+                            };
+                        }
+                        else {
+                            // Executes if item is a file.
+                            processFile(newSrc, newDest, item);
                         };
-                    }
-                    else {
-                        processFile(newSrc, newDest, item);
-                    };
-                });
+                    });
+                };
             });
         },
+
+        // Method to copy files to destination directory. JavaScript files are minified before copying.
         processFile = function (src, dest, item) {
             if (isJsFile(item)) {
+                // If JavaScript file then minify before copying.
                 minifyJs(src, function (error, data) {
                     if (error) {
                         logger(error.message);
                     }
                     else {
+                        // Write minified code into destination file.
                         fs.writeFile(dest, data, function (err, fileRef) {
                             if (err) {
                                 logger(err.message);
                             }
                             else {
-                                logger(src + ' JS file minified Successfully!!!');
+                                logger(src + ' - file minified and copied.');
                             };
                         });
                     };
                 });
             }
             else {
-                (filesToIgnore.indexOf(item) == -1) && fs.createReadStream(src).pipe(fs.createWriteStream(dest));
+                // Copy non JavaScript files to destination.
+                (filesToIgnore.indexOf(item) == -1) && (fs.createReadStream(src).pipe(fs.createWriteStream(dest)), logger(src + ' - file copied.'));
             };
         },
-        startBuild = function (destinationPath) {
+
+        // method to start publish process.
+        startPublish = function (destinationPath) {
             console.log(destinationPath);
             processFolder(sourcePath, destinationPath);
         };
 
+    // Publish process is initiated from here.
     try {
         var destinationPath = process.argv[2] ? process.argv[2].replace('"', '') : dir.replace('tools', 'build');
         createDirectory(destinationPath);
-        startBuild(destinationPath);
+        startPublish(destinationPath);
     }
     catch (err) {
-        logger(err);
+        logger(err.message);
     }
 
 })(require, process);
